4e466485465871db54ead800caf3505917262208
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-12 19:20:49 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,8 +2,20 @@ class Course
   
     attr_accessor :title, :schedule, :description
     
+    @@all = []
     
-    def
+    def initialize
+      @@all << self
+    end
+    
+    def self.all
+      @@all
+    end
+    
+    def self.reset.all
+      @@all.clear
+    end
+    
     
 end
 
